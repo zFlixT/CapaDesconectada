bd11834386e0fe25187a2006e9222c2fb7037bde
zFlixT <pt0109032023@unab.edu.sv>
2024-10-14 20:26:12 -0600

    Realización de la guía XXXI: Data set Tipado resumen

---
+++ CapaDesconectada/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDesconectada
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwind.Customers' Puede moverla o quitarla según sea necesario.
+            this.customersTableAdapter.Fill(this.northwind.Customers);
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            customersBindingSource.AddNew();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            customersBindingSource.EndEdit();
+            customersTableAdapter.Update(northwind);
+            MessageBox.Show("Guardado");
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            customersBindingSource.RemoveCurrent();
+        }
+    }
+}
--- CapaDesconectada/Program.cs
+++ CapaDesconectada/Program.cs
@@ -16,7 +16,7 @@ namespace CapaDesconectada
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
